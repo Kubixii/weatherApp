daa2c6c829287faf67a70eda4333580bed08e989
Kubixii <39913230+Kubixii@users.noreply.github.com>
2023-02-18 15:41:58 +0100

    updated

--- src/store/StoreProvider.jsx
+++ src/store/StoreProvider.jsx
@@ -1,39 +1,32 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { defaultParams, initData } from '../helpers/defaults'
 
-import { weatherRequest } from '../helpers/requests'
-
 export const StoreContext = createContext(null)
 
 const StoreProvider = ({ children }) => {
 
     const [currentParams, setCurrentParams] = useState(defaultParams)
+    const [currentLocation, setCurrentLocation] = useState({})
     const [weatherData, setWeatherData] = useState(initData)
 
-    const getWeatherData = async (lat, lon, type) => {
+    //TODO docelowo paramsy z react router jak niżej
+    // URL/lat/lon/typ_wyświetlania
+    // typy wyświetlania w <Outlet/> jako osobne zakładki
+    // zrobić pobieranie danych o lokalizacji na podstawie paramsów
 
-        //TODO docelowo paramsy z react router jak niżej
-        // URL/lat/lon/typ_wyświetlania
-        // typy wyświetlania w <Outlet/> jako osobne zakładki
-
-        await weatherRequest.get(`forecast?latitude=${lat}&longitude=${lon}&${type}=temperature_2m`)
-            .then(res => {
-                setWeatherData(prev => { return { ...prev, [type]: res.data } })
-            })
+    const updateWeatherData = (data, type) => setWeatherData(prev => { return { ...prev, [type]: data } })
+    const updateParams = (city, lat, lon, type) => {
+        setCurrentLocation(city)
+        setCurrentParams({ lat, lon, type })
     }
-    useEffect(() => {
-        const { lat, lon, type } = currentParams
-        getWeatherData(lat, lon, type)
-    }, [currentParams.lat, currentParams.lon])
-
-    const updateParams = (lat, lon, type) => setCurrentParams({ lat, lon, type })
 
     return (
         <StoreContext.Provider value={{
             currentParams,
             weatherData,
-            getWeatherData,
-            updateParams
+            currentLocation,
+            updateParams,
+            updateWeatherData
         }}>
             {children}
         </StoreContext.Provider>
